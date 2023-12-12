4cc9561807b246335c58ae19a72a0fa2f52f6b7d
Jochen Saige <Jochen.Saige@auticon.de>
2023-12-12 15:00:43 +0100

    split

--- python/change_milestone_description_request.py
+++ python/change_milestone_description_request.py
@@ -10,6 +10,8 @@ jira_instance_url = os.getenv('JIRA_BASE_URL')
 issue = os.getenv('ISSUE')  # get the issue
 due_date = os.getenv('DUE_DATE')  # get the due date
 
+new_description = new_description.split('AUTOMATICALLY GENERATED CONTENT DO NOT EDIT/DELETE BEYOND THIS POINT')[0]
+
 # create the JSON data
 data = {
     "fields": {
@@ -20,7 +22,7 @@ data = {
 
 # make the PUT request
 response = requests.put(
-    f"{jira_instance_url}rest/api/2/issue/{issue}",
+    f"https:{jira_instance_url}rest/api/2/issue/{issue}",
     data=json.dumps(data),
     headers={"Content-Type": "application/json"},
     auth=(jira_user_email, jira_api_token)
