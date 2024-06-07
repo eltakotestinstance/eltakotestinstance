973baaa062d08fcddb29cc94fb90b4a33b042eb3
Jochen Saige <Jochen.Saige@auticon.de>
2024-06-07 07:54:55 +0200

    `chore: Update environment variable access in issue_generate_secrets_api_key_check.py and issue_generate_secrets_api_mail_check.py`

--- python/issue_generate_secrets_api_key_check.py
+++ python/issue_generate_secrets_api_key_check.py
@@ -3,12 +3,12 @@ import os
 USER_NAME = os.getenv('USER_NAME')
 
 action_content = f"""
-        if [[ -z "${{{{ secrets.JIRA_API_TOKEN_{user_name}" }}}} ]]; then
-          echo "JIRA secret for {user_name} is not set"
+        if [[ -z "${{{{ secrets.JIRA_API_TOKEN_{USER_NAME} }}}}"  ]]; then
+          echo "JIRA secret for {USER_NAME} is not set"
           OUTPUT="{{{{secrets.JIRA_API_TOKEN}}}}"
         else
-          echo "JIRA secret for {user_name} is set"
-          OUTPUT="{{{{secrets.JIRA_API_TOKEN_{user_name}}}}}"
+          echo "JIRA secret for {USER_NAME} is set"
+          OUTPUT="{{{{secrets.JIRA_API_TOKEN_{USER_NAME}}}}}"
         fi
 """
 
--- python/issue_generate_secrets_api_mail_check.py
+++ python/issue_generate_secrets_api_mail_check.py
@@ -3,12 +3,12 @@ import os
 USER_NAME = os.getenv('USER_NAME')
 
 action_content = f"""
-        if [[ -z "${{{{ secrets.JIRA_USER_EMAIL_{user_name}" }}}} ]]; then
-          echo "JIRA secret for {user_name} is not set"
+        if [[ -z "${{{{ secrets.JIRA_USER_EMAIL_{USER_NAME} }}}}"  ]]; then
+          echo "JIRA secret for {USER_NAME} is not set"
           OUTPUT="{{{{secrets.JIRA_USER_EMAIL}}}}"
         else
-          echo "JIRA secret for {user_name} is set"
-          OUTPUT="{{{{secrets.JIRA_USER_EMAIL_{user_name}}}}}"
+          echo "JIRA secret for {USER_NAME} is set"
+          OUTPUT="{{{{secrets.JIRA_USER_EMAIL_{USER_NAME}}}}}"
         fi
 """
 
