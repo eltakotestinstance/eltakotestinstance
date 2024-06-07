3702a48db5bd30a62f59c081476d1cf34bd01367
Jochen Saige <Jochen.Saige@auticon.de>
2024-06-07 07:41:52 +0200

    chore: Update GitHub workflow to check for JIRA secret

---
+++ python/issue_generate_secrets_api_key_check.py
@@ -0,0 +1,14 @@
+
+USER_NAME = int(os.getenv('USER_NAME'))
+
+action_content = f"""
+        if [[ -z "${{{{ secrets.JIRA_API_TOKEN_{user_name}" }}}} ]]; then
+          echo "JIRA secret for {user_name} is not set"
+          OUTPUT="{{{{secrets.JIRA_API_TOKEN}}}}"
+        else
+          echo "JIRA secret for {user_name} is set"
+          OUTPUT="{{{{secrets.JIRA_API_TOKEN_{user_name}}}}}"
+        fi
+"""
+
+print(action_content)
---
+++ python/issue_generate_secrets_api_mail_check.py
@@ -0,0 +1,14 @@
+
+USER_NAME = int(os.getenv('USER_NAME'))
+
+action_content = f"""
+        if [[ -z "${{{{ secrets.JIRA_USER_EMAIL_{user_name}" }}}} ]]; then
+          echo "JIRA secret for {user_name} is not set"
+          OUTPUT="{{{{secrets.JIRA_USER_EMAIL}}}}"
+        else
+          echo "JIRA secret for {user_name} is set"
+          OUTPUT="{{{{secrets.JIRA_USER_EMAIL_{user_name}}}}}"
+        fi
+"""
+
+print(action_content)
