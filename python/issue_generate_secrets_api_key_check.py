6bfded08187f53307338def9cfff87aecfd7ddd0
Jochen Saige <Jochen.Saige@auticon.de>
2024-06-07 08:05:12 +0200

    `chore: Update GitHub workflow to check for JIRA secret`

--- python/issue_generate_secrets_api_key_check.py
+++ python/issue_generate_secrets_api_key_check.py
@@ -2,14 +2,6 @@ import os
 
 USER_NAME = os.getenv('USER_NAME')
 
-action_content = f"""
-        if [[ -z "${{{{ secrets.JIRA_API_TOKEN_{USER_NAME} }}}}"  ]]; then
-          echo "JIRA secret for {USER_NAME} is not set"
-          OUTPUT="{{{{secrets.JIRA_API_TOKEN}}}}"
-        else
-          echo "JIRA secret for {USER_NAME} is set"
-          OUTPUT="{{{{secrets.JIRA_API_TOKEN_{USER_NAME}}}}}"
-        fi
-"""
+action_content = f"${{{{ secrets.JIRA_API_TOKEN_{USER_NAME} }}}}"
 
 print(action_content)
