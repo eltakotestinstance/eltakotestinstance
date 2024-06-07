1503dc5f64cbbcf17d5856aea9854652a3faf198
Jochen Saige <Jochen.Saige@auticon.de>
2024-06-07 08:20:14 +0200

    `chore: Update GitHub workflow to check for JIRA secret`

--- python/issue_generate_secrets_api_key_check.py
+++ python/issue_generate_secrets_api_key_check.py
@@ -1,7 +1,20 @@
+from github import Github
 import os
 
+# First create a Github instance:
+
+# using an access token
+g = Github(os.getenv("GITHUB_TOKEN"))
 USER_NAME = os.getenv('USER_NAME')
 
-action_content = f"${{{{ secrets.JIRA_API_TOKEN_{USER_NAME} }}}}"
 
-print(action_content)
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    if repo.name == "eltakotestinstance":
+        secrets = repo.get_secrets()
+        for secret in secrets:
+            if secret.name == f"JIRA_API_TOKEN_{os.getenv('USER_NAME')}":
+                print("Secret exists")
+                break
+        else:
+            print("Secret does not exist")
